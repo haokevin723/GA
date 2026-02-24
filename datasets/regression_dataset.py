4182f8324743441366b5a5022f1b774a59941b72
haokk <haokkuan@gmail.com>
2026-02-24 21:55:08 +0800

    modify the path of data

--- datasets/regression_dataset.py
+++ datasets/regression_dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 class RegressionDataset(Dataset):
     def __init__(self, data_dir, label_csv, split_txt, transform=None):
         self.data_dir = data_dir
-        self.labels = pd.read_csv(label_csv, index_col='file')
+        self.labels = pd.read_csv(label_csv, index_col='png_name')
         with open(split_txt, 'r') as f:
             self.file_list = [line.strip() for line in f if line.strip() and not line.startswith('#')]
         self.transform = transform
@@ -18,7 +18,7 @@ class RegressionDataset(Dataset):
         img_name = self.file_list[idx]
         img_path = os.path.join(self.data_dir, img_name)
         image = Image.open(img_path).convert('RGB')
-        label = float(self.labels.loc[img_name, 'week_value'])
+        label = float(self.labels.loc[img_name, 'gest_week_days'])
         if self.transform:
             image = self.transform(image)
         return image, label
